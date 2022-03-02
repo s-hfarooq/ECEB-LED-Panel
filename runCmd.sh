21527a660b46a59ac088c6971b902ed88d00426d
Hassan Farooq <1hassanfarooq@gmail.com>
2022-03-02 17:20:36 -0600

    Testing

--- runCmd.sh
+++ runCmd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo --led-cols=128 --led-daemon -m 75 -D1 /home/pi/ECEB-LED-Panel/imgs/8-23.ppm
+sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo --led-cols=128 --led-daemon -m 75 -D1 /home/pi/ECEB-LED-Panel/imgs/Untitled.ppm
