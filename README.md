3b13396eae8b967ad6b0c50c409105d0eaa4852c
Hassan Farooq <1hassanfarooq@gmail.com>
2022-03-02 16:59:37 -0600

    New script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ECEB-LED-Panel
+
+
+Repo for images shown on LED panel in ECE 110 Lab room as seen from the ECEB Atrium. 
+
+
+To change image: Upload a ppm file into the `imgs` folder and change the image name found in `runCmd.sh`. The Pi automatically checks for updates every 10 seconds. 
--- pullScript.sh
+++ pullScript.sh
@@ -2,6 +2,9 @@
 
 cd /home/pi/ECEB-LED-Panel
 
+nohup ./runCmd.sh &
+echo $! > pid_out
+
 while true
 do
     git fetch
@@ -10,8 +13,13 @@ do
 
     if [ "$HEADHASH" != "$UPSTREAMHASH" ]
         then
-            echo -e ${ERROR}Not up to date with origin. Aborting.${NOCOLOR}
+            echo -e ${ERROR}Not up to date with origin. Pulling.${NOCOLOR}
             git pull
+            # restart script
+            pidVal=$(<pid_out)
+            kill $pidVal
+            nohup ./runCmd.sh &
+            echo $! > pid_out
             echo
         else
             echo -e ${FINISHED}Current branch is up to date with origin/master.${NOCOLOR}
